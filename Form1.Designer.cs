fa9a5332be235a7cad3ff4e01414cdc758126b42
Ilya Kavalevich <kava2000.ilia@gmail.com>
2020-05-27 00:00:39 +0300

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace OSSP_Lab_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.buttonAddProcess = new System.Windows.Forms.Button();
+            this.numbersOfProceses = new System.Windows.Forms.TextBox();
+            this.timeQ = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(11, 85);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(192, 50);
+            this.buttonStart.TabIndex = 2;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Location = new System.Drawing.Point(594, 85);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(192, 50);
+            this.buttonStop.TabIndex = 3;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            // 
+            // buttonAddProcess
+            // 
+            this.buttonAddProcess.Location = new System.Drawing.Point(304, 85);
+            this.buttonAddProcess.Name = "buttonAddProcess";
+            this.buttonAddProcess.Size = new System.Drawing.Size(192, 50);
+            this.buttonAddProcess.TabIndex = 4;
+            this.buttonAddProcess.Text = "Add process";
+            this.buttonAddProcess.UseVisualStyleBackColor = true;
+            // 
+            // numbersOfProceses
+            // 
+            this.numbersOfProceses.Location = new System.Drawing.Point(162, 13);
+            this.numbersOfProceses.Name = "numbersOfProceses";
+            this.numbersOfProceses.Size = new System.Drawing.Size(100, 22);
+            this.numbersOfProceses.TabIndex = 5;
+            // 
+            // timeQ
+            // 
+            this.timeQ.Location = new System.Drawing.Point(162, 41);
+            this.timeQ.Name = "timeQ";
+            this.timeQ.Size = new System.Drawing.Size(100, 22);
+            this.timeQ.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 17);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Number of processes";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(127, 17);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Time quantum size";
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(11, 163);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(775, 325);
+            this.listView1.TabIndex = 9;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 500);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.timeQ);
+            this.Controls.Add(this.numbersOfProceses);
+            this.Controls.Add(this.buttonAddProcess);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.Button buttonAddProcess;
+        private System.Windows.Forms.TextBox numbersOfProceses;
+        private System.Windows.Forms.TextBox timeQ;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
+
